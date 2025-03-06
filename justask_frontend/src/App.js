4a94dba68ada7810dd6289ec6346db47a0dcb08f
pratiksha <php_team@mindspacetech.com>
2025-03-06 15:51:56 +0530

    added frontend project

---
+++ justask_frontend/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from './pages/login';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path='/LoginPage' element={<Login />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
